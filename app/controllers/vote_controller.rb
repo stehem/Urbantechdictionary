c05fa97ee0f11ebe3a61d8650f68a3b4fdc2f745
stephane <stephane@stephane-VPCYB1S1E.(none)>
2011-03-31 11:07:22 +0200

    31

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -4,23 +4,19 @@ respond_to :js
 
 def create
   definition = Definition.find(params[:def_id])
-  case params[:vtype]
-    when "p"
-      if !Vote.has_voted(params[:def_id],request.remote_ip)
+  if Vote.has_voted(params[:def_id],request.remote_ip)
+    render :json => {:result => "fail"}
+  else
+    case params[:vtype]
+      when "p"
         definition.increment!(:upv)
         definition.votes.create(:ip => request.remote_ip)
         render :nothing => true
-      else
-        render :json => {:result => "fail"}
-      end
-    when "n"
-      if !Vote.has_voted(params[:def_id],request.remote_ip)
+      when "n"
         definition.increment!(:dwv)
         definition.votes.create(:ip => request.remote_ip)
         render :nothing => true
-      else
-        render :json => {:result => "fail"}
-      end
+    end
   end
 end
 
